96e31fc6ff0da1cf8518393d16434789224d42f5
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-12-10 09:22:51 -0800

    finished bread functionality

--- PierresBakery.Solution/PierresBakery/Models/Item.cs
+++ PierresBakery.Solution/PierresBakery/Models/Item.cs
@@ -6,7 +6,19 @@ namespace PierresBakery.Models
   public class Bread{
     public static int CheckBreadPrice(int input)
     {
-      return input;
+      int price = 0;
+      for(int i = 1; i <= input; i++)
+      {
+        if(i % 3 == 0)
+        {
+          price += 0;
+        }
+        else
+        {
+          price += 5;
+        }
+      }
+      return price;
     }
   }
 }
